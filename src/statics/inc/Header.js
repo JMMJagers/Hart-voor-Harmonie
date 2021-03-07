498066d5ca66fc2f5863fdfec05b957e2cfd7841
JMMJagers <jmmjagers@gmail.com>
2021-03-07 17:38:04 +0100

    remove unused JS

--- src/statics/inc/Header.js
+++ src/statics/inc/Header.js
@@ -11,8 +11,11 @@ const Header = () => {
             <div className="col-8 offset-2">
               <div className="site-branding-container">
                 <div className="site-branding">
-                  <nav className="navbar navbar-expand-lg">
+                  <nav className="navbar navbar-expand-lg sticky-top">
                     <a className="logo" href="/"></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                      <span class="navbar-toggler-icon"></span>
+                    </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul className="navbar-nav mr-auto">
                         {navItems.navItems.map((item) =>
--- src/statics/inc/ProductCard.js
+++ src/statics/inc/ProductCard.js
@@ -4,13 +4,13 @@ import {Link} from "react-router-dom";
 const ProductCard = ({name, price, prevprice, img}) => {
 
   return (
-    <div className="col-md-4 selected_vid">
+    <div className="col-lg-4 col-md-6 col-12">
       <div className="card text-center text-white bg-primary m-3 m-3">
         <img src={img} className="product_img"/>
         <div className="card-body">
-          <h5 className="card-title">{ name }</h5>
-          <i className="prev-price mb-0">{ prevprice }</i>
-          <p>{ price }</p>
+          <h5 className="card-title">{name}</h5>
+          <i className="prev-price mb-0">{prevprice}</i>
+          <p>{price}</p>
         </div>
       </div>
     </div>
