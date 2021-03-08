86878073ae17dfd73b2e8360634c60f9ba831ec1
JMMJagers <jmmjagers@gmail.com>
2021-03-08 19:43:01 +0100

    fix nav

--- src/statics/inc/Header.js
+++ src/statics/inc/Header.js
@@ -1,18 +1,27 @@
-import React, {Fragment} from 'react';
+import React, {useState, Fragment} from 'react';
 import NavItem from './NavItem';
 import navItems from '../../data/navItems.json';
 
 const Header = () => {
 
+  const [isNavCollapsed, setIsNavCollapsed] = useState(true);
+
+  const handleNavCollapse = () => setIsNavCollapsed(!isNavCollapsed);
+
   return (
 
       <header className="header-container">
-        <div className="col-8 offset-2">
+        <div className="col-12 col-md-8 offset-md-2">
           <div className="site-branding-container">
             <div className="site-branding">
-              <nav className="navbar navbar-expand-lg sticky-top">
+              <nav className="navbar navbar-expand-md sticky-top">
                 <a className="logo" href="/"></a>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <button className="custom-toggler navbar-toggler" type="button" aria-expanded={!isNavCollapsed ? true : false} onClick={handleNavCollapse}>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                  <span className="icon-bar"></span>
+                </button>
+                <div className={`${isNavCollapsed ? 'collapse' : ''} navbar-collapse`}>
                   <ul className="navbar-nav mr-auto">
                     {navItems.navItems.map((item) =>
                       <NavItem url={item.url} title={item.title}/>
@@ -24,6 +33,7 @@ const Header = () => {
           </div>
         </div>
       </header>
+
     )
 }
 
